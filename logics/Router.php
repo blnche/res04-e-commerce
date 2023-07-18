a391c01462594b0641a23ded95adb21b1b94a02f
Kqer35 <kevin.qeraca@gmail.com>
2023-07-18 10:24:40 +0000

    router

---
+++ logics/Router.php
@@ -0,0 +1,16 @@
+<?php 
+
+function checkRoute(string $route){
+    
+    $uc = new UserController;
+    
+    if ($route === 'register') {
+        
+    $uc->register();
+    
+    } else if ($route === 'login') {
+    $uc->login();
+    }
+}
+
+?>
--- models/Products.php
+++ models/Products.php
@@ -14,11 +14,11 @@ class Products{
         $this->category_id = $category_id;
     }
     
-    public function getId(){return $this->id;}
-    public function getName(){ return $this->name;}
-    public function getPrice(){return $this->price;}
-    public function getDescription(){return $this->description;}
-    public function getCategory_id(){return $this->category_id;}
+    public function getId() : ?int {return $this->id;}
+    public function getName() : string { return $this->name;}
+    public function getPrice() : string {return $this->price;}
+    public function getDescription() : string {return $this->description;}
+    public function getCategory_id(): string {return $this->category_id;}
     
     public function setId($id){$this->id = $id;}
     public function setName($name){$this->name = $name;}
