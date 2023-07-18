cadb302a98af69372cc2d31c661edc922c171517
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-07-18 16:14:01 +0200

    Product.php correct

--- models/Product.php
+++ models/Product.php
@@ -6,24 +6,28 @@ class Product{
     private string $price;
     private string $description;
     private int $category_id;
-    
-    public function __construct (string $name, string $price, string $description, int $category_id){
+    private string $url_media;
+
+    public function __construct (string $name, string $price, string $description, int $category_id, string $url_media){
         $this->name = $name;
         $this->price = $price;
         $this->description = $description;
         $this->category_id = $category_id;
+        $this->url_media = $url_media;
     }
-    
+
     public function getId() : ?int {return $this->id;}
     public function getName() : string { return $this->name;}
     public function getPrice() : string {return $this->price;}
     public function getDescription() : string {return $this->description;}
-    public function getCategory_id(): string {return $this->category_id;}
-    
+    public function getCategoryId(): string {return $this->category_id;}
+    public function getUrlMedia(): string {return $this->url_media;}
+
     public function setId($id){$this->id = $id;}
     public function setName($name){$this->name = $name;}
     public function setPrice($price){$this->price = $price;}
     public function setDescription($description){$this->description = $description;}
-    public function setCategory_id($category_id){$this->category_id = $category_id;}
-    
+    public function setCategoryId($category_id){$this->category_id = $category_id;}
+    public function setUrlMedia($url_media){$this->url_media = $url_media;}
+
 }
