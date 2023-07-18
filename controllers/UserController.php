17b8d55cda3554c62223158b62408f1e63c0dce7
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-07-18 12:05:13 +0200

    next

---
+++ controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+class UserController extends AbstractController
+{
+
+    public function login()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function remove()
+    {
+
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'logics/Router.php';
+
+require 'models/Adresse.php';
+require 'models/Category.php';
+require 'models/User.php';
+require 'models/Order.php';
+require 'models/Products.php';
+
+// require 'managers/AbstractManager.php';
+require 'managers/UserManager.php';
+
+// require 'controllers/AbstractController.php';
+require 'controllers/UserController.php';
+
+
