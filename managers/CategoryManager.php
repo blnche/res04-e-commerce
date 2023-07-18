fba36256da6f91b3e61fdb061975b6a5c1f25c93
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-07-18 14:21:37 +0200

    CategoryManager.php

--- managers/CategoryManager.php
+++ managers/CategoryManager.php
@@ -6,7 +6,12 @@
             $query = $this->db->prepare("SELECT * FROM categories");
             $query->execute();
             $categories = $query->fetchAll(PDO::FETCH_ASSOC);
-            return $categories;
+            $categoriesTab = [];
+            foreach($categories as $category){
+                $categoryInstance = new Category($category["name"]);
+                array_push($categoriesTab, $categoryInstance);
+            }
+            return $categoriesTab;
         }
     }
 ?>
