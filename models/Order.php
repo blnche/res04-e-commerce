fb18a8eeb026c1b4fcf6acf877705ce5fe9f8dba
KodingCore <131252253+KodingCore@users.noreply.github.com>
2023-07-18 13:47:38 +0200

    order correct

--- models/Order.php
+++ models/Order.php
@@ -2,21 +2,24 @@
     class Order{
     private ?int $id;
     private string $status;
-    private Date $date;
+    private $date;
     private int $user_id;
     
-    public function __construct (string $status, Date $date, int $user_id){
+    public function __construct (string $status, DateTime $date, int $user_id){
+        $this->status = $status;
+        $this->date = new DateTime();
+        $this->user_id = $user_id;
     }
     
     public function getId(){return $this->id;}
     public function getStatus(){ return $this->status;}
-    public function getDate(){ return $this->Date;}
+    public function getDate(){ return $this->date;}
     public function getUserId(){return $this->user_id;}
 
     
     public function setId($id){$this->id = $id;}
-    public function setStatus($Status){$this->status = $status;}
-    public function setDate($date){$this->Date = $date;}
-    public function setUserId($user_Id){$this->user_id = $user_id;}
+    public function setStatus($status){$this->status = $status;}
+    public function setDate($date){$this->date = $date;}
+    public function setUserId($user_id){$this->user_id = $user_id;}
     }
 ?>
